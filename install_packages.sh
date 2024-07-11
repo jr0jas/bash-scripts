0526699e863416cb9fb94419d2d89ebc217a7c37
Jorge Rojas <jrojas.pz@gmail.com>
2024-07-11 14:11:10 -0600

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -13,9 +13,19 @@ folder_path=${1:-$(pwd)}
 # Change to the specified folder
 cd "$folder_path" || { echo -e "${RED}Folder not found${NC}"; exit 1; }
 
+# Get the list of zip files in the folder
+packages=( ./*.zip )
+
+# Get the total number of packages
+total_packages=${#packages[@]}
+
+# Initialize the counter
+counter=0
+
 # Loop through each zip file in the folder and run the curl command
-for package in ./*.zip; do
-  echo -e "\nInstalling package: $package"
+for package in "${packages[@]}"; do
+  ((counter++))
+  echo -e "\nInstalling package $counter of $total_packages: $package"
   
   # Send the package and capture the HTTP response code
   response=$(curl -s -o /dev/null -w "%{http_code}" -u admin:admin -F file=@"$package" -F name="content-dam" -F force=true -F install=true http://localhost:4502/crx/packmgr/service.jsp)
@@ -26,5 +36,8 @@ for package in ./*.zip; do
   else
     echo -e "${RED}Installation failed for package: $package (HTTP response code: $response)${NC}\n"
   fi
+  
+  # Calculate and display the percentage of packages installed
+  percentage=$(awk "BEGIN { pc=($counter/$total_packages)*100; printf \"%.2f\", pc }")
+  echo -e "Progress: $percentage% ($counter of $total_packages packages installed)\n"
 done
-
